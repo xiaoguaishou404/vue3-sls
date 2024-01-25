81fa8fb76bef80e2c694f26bb27a0efd16f3abaf
slsMac <2054376909@qq.com>
2024-01-25 21:14:30 +0800

    eslint plugin:vue/vue3-essential

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,9 +6,20 @@ module.exports = {
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-typescript'
+    '@vue/eslint-config-typescript',
+    'plugin:vue/vue3-strongly-recommended'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    "vue/max-attributes-per-line": ["error", {
+      "singleline": {
+        "max": 20
+      },
+      "multiline": {
+        "max": 20
+      }
+    }],
   }
 }
